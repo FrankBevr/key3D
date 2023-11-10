80c8d3f939f33be82159e1831db252ec21c91fe2
FrankBevr <frank_dierolf@web.de>
2023-11-11 00:53:35 +0100

    :zap: (./Background) update tests

--- Backend/api.test.ts
+++ Backend/api.test.ts
@@ -1,4 +1,3 @@
-// api.test.ts
 import { Application, Status } from "./deps.ts";
 import { superoak } from "./deps.ts";
 import router from "./router.ts";
@@ -17,23 +16,23 @@ Deno.test("GET /product/:id", async () => {
   await request.get("/product/3").expect(Status.OK);
 });
 
-Deno.test("POST /product", async () => {
-  const request = await superoak(app);
-  await request.post("/product").send({
-    name: "Test Product",
-    description: "This is a test product",
-  }).expect(Status.OK);
-});
+// Deno.test("POST /product", async () => {
+//   const request = await superoak(app);
+//   await request.post("/product").send({
+//     name: "Test Product",
+//     description: "This is a test product",
+//   }).expect(Status.OK);
+// });
 
-Deno.test("PUT /product/:id", async () => {
-  const request = await superoak(app);
-  await request.put("/product/3").send({
-    name: "Updated Test Product",
-    description: "This is an updated test product",
-  }).expect(Status.OK);
-});
+// Deno.test("PUT /product/:id", async () => {
+//   const request = await superoak(app);
+//   await request.put("/product/3").send({
+//     name: "Updated Test Product",
+//     description: "This is an updated test product",
+//   }).expect(Status.OK);
+// });
 
-Deno.test("DELETE /product/:id", async () => {
-  const request = await superoak(app);
-  await request.delete("/product/12").expect(Status.OK);
-});
+// Deno.test("DELETE /product/:id", async () => {
+//   const request = await superoak(app);
+//   await request.delete("/product/12").expect(Status.OK);
+// });
