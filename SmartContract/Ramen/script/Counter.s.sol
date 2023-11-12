b333a61cbe0bf942290157606aadda3736f34709
FrankBevr <frank_dierolf@web.de>
2023-11-12 19:06:49 +0100

    :zap: (./SmartContract) deploy goerli via script

--- SmartContract/Ramen/script/Counter.s.sol
+++ SmartContract/Ramen/script/Counter.s.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.23;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
-
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Counter nft = new Counter();
+
+        vm.stopBroadcast();
     }
 }
