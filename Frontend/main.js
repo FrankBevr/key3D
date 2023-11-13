344596c44182f3f27d7d23e23d80a1a5df150b5d
FrankBevr <frank_dierolf@web.de>
2023-11-13 09:45:22 +0100

    :zap: (./frontend) get ethers running

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,4 +1,30 @@
+import { sign } from "crypto";
 import "./style.css";
+import { ethers, formatEther, parseEther } from "ethers";
+
+async function testo() {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  console.log(await provider.send("eth_requestAccounts", []));
+  console.log(await provider.getBlockNumber());
+  console.log(await provider.getBalance("ethers.eth"));
+  const balance = await provider.getBalance(
+    "0xeC3417669F881720A6131f4ecf4ED6F38f6e7450"
+  );
+  console.log(formatEther(balance));
+  console.log(parseEther("1.0"));
+  const signer = await provider.getSigner();
+
+  const tx = await signer.sendTransaction({
+    to: "0x9C49f71e66e089584e05E9eAce2C6caD783E9fE4",
+    value: parseEther("0.0001"),
+  });
+
+  // Often you may wish to wait until the transaction is mined
+  const receipt = await tx.wait();
+  console.log(receipt);
+
+}
+testo();
 
 document.querySelector("#app").innerHTML = `
   <div>
