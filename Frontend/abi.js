9a7616aba6c9e4fc5dc8cc3d89f7da9608643263
FrankBevr <frank_dierolf@web.de>
2023-11-14 10:46:52 +0100

    :zap: (./Frontend) getNumber from Contract

---
+++ Frontend/abi.js
@@ -0,0 +1,13 @@
+export const abi = [
+  // Some details about the token
+  "function number() view returns (uint256)",
+
+  // Get the account balance
+  "function setNumber(uint256 newNumber) public",
+
+  // Send some of your tokens to someone else
+  "function increment() public",
+
+  // Send some of your tokens to someone else
+  "function increment2() public",
+];
